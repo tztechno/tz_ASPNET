583f2a9002f50cfd2469beb309f15536d01076f2
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-13 12:11:25 +0900

    Create Default.aspx.cs

---
+++ 00/Default.aspx.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+
+namespace YourNamespace
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // ページがロードされたときに実行されるコード
+        }
+    }
+}
